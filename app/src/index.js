9c96fdc98c3864639aa6a4ea06fcf5480421df9e
supercodingfan <supercodingfan@gmail.com>
2019-06-07 12:25:21 +0300

    Run application

--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { HashRouter } from 'react-router-dom'
-import { Provider } from 'react-redux';
-import createHistory from 'history/createBrowserHistory';
-import configureStore from './store';
-import Root from './root';
+import App from './App';
+// import { HashRouter } from 'react-router-dom'
+// import { Provider } from 'react-redux';
+// import createHistory from 'history/createBrowserHistory';
+// import configureStore from './store';
+// import Root from './root';
 import './styles/main.scss';
-export const history = createHistory();
-const store = configureStore();
-render((
-  <Provider store={store} history={history} >
-    <HashRouter>
-      <Root />
-    </HashRouter>
-  </Provider>
-), document.getElementById('root'));
+// export const history = createHistory();
+// const store = configureStore();
+render((<App/>), document.getElementById('root'));
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,19 +1,37 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
-  template: './src/index.html',
-  filename: './index.html'
+    template: './public/index.html',
+    filename: './index.html'
 });
 const miniCssExtractPlugin = new MiniCssExtractPlugin({
-  filename: "[name].css",
-  chunkFilename: "[id].css"
+    filename: "[name].css",
+    chunkFilename: "[id].css"
 });
 module.exports = {
- module: {
-   rules: []
- },
- plugins: [htmlWebpackPlugin, miniCssExtractPlugin],
- resolve: {
-   extensions: ['.js', '.jsx'],
- }
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [htmlWebpackPlugin, miniCssExtractPlugin],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    }
 };
