5321aa84a0cfeaff60480427cfe208fb3055885a
supercodingfan <supercodingfan@gmail.com>
2019-07-12 10:31:37 +0300

    Added quick assign markup

--- front/src/components/Ticket.jsx
+++ front/src/components/Ticket.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
 import {STATUSES_LIST} from "../constants/status-types";
 import {useDispatch} from "react-redux";
@@ -7,7 +7,23 @@ import {CHANGE_STATUS} from "../constants/action-types";
 const Ticket = (ticket) => {
 
     const [status, setStatus] = useState(ticket.status);
+    const [assignee, setAssignee] = useState(ticket.user ? ticket.user.id : '');
+    const [users, setUsers] = useState([]);
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchAvailableUsers = async () => {
+            const fetchResult = await fetch('http://0.0.0.0:3000/users', {
+                method: 'GET',
+                mode: 'cors',
+                headers: {'Content-Type': 'application/json'}
+            });
+            return await fetchResult.json();
+        };
+
+        fetchAvailableUsers().then(result =>
+            setUsers(result.users)).catch(reason => console.log(reason.message));
+    }, []);
 
     const changeStatus = (status) => {
         let ticketToUpdate = Object.assign({}, ticket, {
@@ -36,16 +52,44 @@ const Ticket = (ticket) => {
                 <Link to={`/ticket/${ticket.id}`}>
                     <h5 className="card-title">{ticket.summary}</h5>
                 </Link>
-                <select value={status} onChange={(event) => {
-                    changeStatus(event.target.value);
-                    setStatus(event.target.value)
-                }}>
-                    {STATUSES_LIST.map((status) => (
-                        <option key={status} value={status}>
-                            {status}
-                        </option>
-                    ))}
-                </select>
+                <div className="form-group">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <label htmlFor="status">Status:</label>
+                        </div>
+                        <div className="col-md-6">
+                            <select value={status} onChange={(event) => {
+                                changeStatus(event.target.value);
+                                setStatus(event.target.value)
+                            }} id="status">
+                                {STATUSES_LIST.map((status) => (
+                                    <option key={status} value={status}>
+                                        {status}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <label htmlFor="assignee">Assignee:</label>
+                        </div>
+                        <div className="col-md-6">
+                            <select value={assignee} onChange={(event) => console.log(event.target.value)} id="assignee">
+                                <option value="">
+                                    Unassigned
+                                </option>
+                                {users.map((user) => (
+                                    <option key={user.id} value={user.id}>
+                                        {user.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
