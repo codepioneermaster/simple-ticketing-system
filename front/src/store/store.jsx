1776d0ab6f8f9cc588b151280569ea40ff8d816f
supercodingfan <supercodingfan@gmail.com>
2019-07-12 14:01:30 +0300

    Removed unused thunk

--- front/src/store/store.jsx
+++ front/src/store/store.jsx
@@ -1,6 +1,5 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import { createStore } from "redux";
 import rootReducer from "../reducers/root-reducer";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer);
 export default store;
